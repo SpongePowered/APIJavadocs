697ab34914e3ab3867fd853afa6bf9d036febe5b
Spongie <staff@spongepowered.org>
2022-10-17 04:21:04 +0000

    Publishing javadoc for SpongePowered/plugin-meta 0.8.1-SNAPSHOT

--- mcmod-info/0.8.1-SNAPSHOT/package-search-index.js
+++ mcmod-info/0.8.1-SNAPSHOT/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.spongepowered.plugin.meta"},{"l":"org.spongepowered.plugin.meta.gson"},{"l":"org.spongepowered.plugin.meta.version"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.spongepowered.plugin.meta"},{"l":"org.spongepowered.plugin.meta.gson"},{"l":"org.spongepowered.plugin.meta.version"}];updateSearchResults();
